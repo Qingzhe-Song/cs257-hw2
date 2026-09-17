ad7555bc7ac1514ab939122dddc626d9cb44d5cf
Qingzhe-Song <song.wesley1018@gmail.com>
2026-09-16 22:21:24 -0400

    created README

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -30,5 +30,25 @@ public class PlayerMovement : MonoBehaviour
     {
         transform.Translate(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
         transform.Rotate(0, lookValue * Time.deltaTime, 0);
-    } 
+
+        // if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        // {
+        //     transform.Translate(0, 0, speed * Time.deltaTime);
+        // }
+        // if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        // {
+        //     transform.Translate(0, 0, -speed * Time.deltaTime);
+        // }
+        // if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     transform.Translate(-speed * Time.deltaTime, 0, 0);
+        // }
+        // if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     transform.Translate(speed * Time.deltaTime, 0, 0);
+        // }
+
+        // float mouseX = Input.GetAxis("Mouse X");
+        // transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
+    }
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+I completed everything instructed in Chapters 1, 2, 5, and 6.
+
+If you open the project and it starts in the sample scene, please switch to the included **"main"** scene. That scene contains all of my completed work.
+
+I left several pieces of code, such as the testing code in `Update()`, `Start()`, and `OnDestroy()`, in the project to demonstrate that I completed those portions of the assignment.
+
+I also commented out the previous movement code rather than deleting it. This is to show that I completed the original movement implementation while also incorporating the new movement system into the same script file.
+
+I also included different prefabs based on the prefab variants, as shown by the models on the right side of the scene when loaded in.
